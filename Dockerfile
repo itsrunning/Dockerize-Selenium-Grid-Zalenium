5a4abcf0d4bae078abdfa17487a5678e40c19fbd
valavanvidhya <valavanvidhya@gmail.com>
2019-03-21 15:59:25 +0530

    tests

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM maven:3.6.0-jdk-8-alpine
+ADD . .
+ARG    xml
+ENV xml=$xml
+CMD ["echo", "${xml}"]
+CMD ["mvn","install"]
+CMD ["mvn","compile"]
+CMD ["mvn","package"]
---
+++ src/test/java/GoogleHomepage.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class GoogleHomepage {
+
+
+    @Test
+    public void TestOne() throws MalformedURLException {
+
+       // System.setProperty("webdriver.chrome.driver", "/Users/vidhyat/Desktop/Tests/chromedriver");
+       //  WebDriver driver = new ChromeDriver();
+        URL local = new URL("http://172.17.0.2:4444/wd/hub");
+        WebDriver driver = new RemoteWebDriver(local, DesiredCapabilities.chrome());
+        String baseUrl = "https://www.google.com/";
+        System.out.println("This is instance 2");
+        driver.get(baseUrl);
+        driver.close();
+
+
+
+    }
+}
